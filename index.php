21c01f111fb724ed4e5563262e3c91ca3b911652
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2022-01-05 21:56:36 +0900

    test

--- index.php
+++ index.php
@@ -1,4 +1,34 @@
 <?php
-$html = file_get_contents('https://han.gl/934395/stats');
-echo "html: " . $html;
+header('Content-type: image/svg+xml');
+
+$clicks = 0;
+$num;
+
+function getclicks ($url) {
+	global $clicks;
+    global $num;
+	$token = file_get_contents($url);
+	
+	//문자열 시작점 끝점 설정
+	$start ='header-main';
+	$end ='고유클릭';
+
+	//문자열 자르기
+	$startpos = strpos($token, $start) + 12;
+	$endpos = strpos($token, $end) - 1;
+	$num = substr($token, $startpos, $endpos - $startpos);
+}
+
+//활용 예시
+//https://php.server.com/?url1=링크&url2=링크
+$url1 = $_GET['url1'];
+$url2 = $_GET['url2'];
+$url3 = $_GET['url3'];
+
+getclicks($url1);
+getclicks($url2);
+getclicks($url3);
+
+//svg echo
+echo '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" style="display: inline;"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="3em">',$num,'</text></svg>';
 ?>
